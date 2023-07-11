2e6d3439e8d2e3ad45f320133fdab064926fbb61
Patrick Corcoran <patrickcorcoran10@gmail.com>
2023-07-11 17:31:35 -0500

    remove ant d from navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
-// import { Nav } from 'react-bootstrap';
+// import { Layout, Menu } from 'antd';
+import { Nav } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
 import { Link } from 'react-router-dom';
-const { Header } = Layout;
+// const { Header } = Layout;
 
 const NavBar = () => {
   const handleLogoutClick = () => {
@@ -12,35 +12,35 @@ const NavBar = () => {
   };
 
   return (
-    <Layout className="layout">
-      <Header style={{ display: 'flex', justifyContent: 'center' }}>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1">
-            <Link to="/">Home</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/dashboard">Dashboard</Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Link to="/calories">Count my Calories</Link>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Link to="/about-us">About Us</Link>
-          </Menu.Item>
-        </Menu>
-        <div>
-          {Auth.loggedIn() ? (
+    // <Layout className="layout">
+    //   <Header style={{ display: 'flex', justifyContent: 'center' }}>
+    //     <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+    //       <Menu.Item key="1">
+    //         <Link to="/">Home</Link>
+    //       </Menu.Item>
+    //       <Menu.Item key="2">
+    //         <Link to="/dashboard">Dashboard</Link>
+    //       </Menu.Item>
+    //       <Menu.Item key="3">
+    //         <Link to="/calories">Count my Calories</Link>
+    //       </Menu.Item>
+    //       <Menu.Item key="4">
+    //         <Link to="/about-us">About Us</Link>
+    //       </Menu.Item>
+    //     </Menu>
+    //     <div>
+    //       {Auth.loggedIn() ? (
             <>
               <Link onClick={handleLogoutClick}>Logout</Link>
             </>
-          ) : (
-            <Link onClick={() => (window.location.pathname = '/login')}>
-              Login
-            </Link>
-          )}
-        </div>
-      </Header>
-    </Layout>
+    //       ) : (
+    //         <Link onClick={() => (window.location.pathname = '/login')}>
+    //           Login
+    //         </Link>
+    //       )}
+    //     </div>
+    //   </Header>
+    // </Layout>
     
   );
 };
