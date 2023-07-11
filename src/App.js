1b50be8d804fb51810adcb3abb81eeb177a2cc2c
Patrick Corcoran <patrickcorcoran10@gmail.com>
2023-07-11 14:49:49 -0500

    commit of app js with all componenets but navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import './App.css';
-// import Login from './Components/Login';
+import Login from './Components/Login';
 // import Navbar from './Components/Navbar';
-// import Dashboard from "./pages/Dashboard";
-// import Calories from "./pages/Calories";
-// import { Register } from "./Components/Signup";
+import Dashboard from "./pages/Dashboard";
+import Calories from "./pages/Calories";
+import { Register } from "./Components/Signup";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homepage from "./pages/Homepage";
-// import AboutUs from "./pages/AboutUs";
+import AboutUs from "./pages/AboutUs";
 
 const httpLink = createHttpLink({
   uri: 'https://cal-pal-server-273e253c14e5.herokuapp.com/graphql'
@@ -47,12 +47,12 @@ function App() {
             {/* <Navbar /> */}
           </nav>
           <Routes>
-            {/* <Route path='calories' element={<Calories />} /> */}
-            {/* <Route path='login' element={<Login onFormSwitch={toggleForm} />} /> */}
+            <Route path='calories' element={<Calories />} />
+            <Route path='login' element={<Login onFormSwitch={toggleForm} />} />
             <Route path='/' element={<Homepage />} />
-            {/* <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/dashboard' element={<Dashboard />} />
             <Route path='register' element={<Register onFormSwitch={toggleForm} />} />
-            <Route path='/about-us' element={<AboutUs />} /> */}
+            <Route path='/about-us' element={<AboutUs />} />
           </Routes>
         </div>
       </BrowserRouter>
